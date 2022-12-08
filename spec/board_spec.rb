1dd70af51f870e0130ffdfcf295bf03ae6e92ce1
Matt Enyeart <menyeart1@gmail.com>
2022-12-07 20:22:14 -0700

    Add a test for the cells method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,16 @@
 require 'Rspec'
 require './lib/board'
 require './lib/ship'
+require './lib/cell'
 
 describe Board do
   it "exist" do
     board = Board.new
     expect(board).to be_an_instance_of(Board)
   end
+
+  it "contains cells" do
+    board = Board.new
+    expect(board.cells.class).to eq(Hash)
+  end  
 end
