e27f56519f9f4787d6ddc49c85b4763a04dffc1c
Sam Walker <sg.walker327@gmail.com>
2022-12-08 19:18:41 -0700

    Add diagonal placement test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -47,5 +47,12 @@ describe Board do
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
   end
 
+  it "can validate that coordinates are not placed diagonally" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  end
 end
 
