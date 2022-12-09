44cbad7a841920702741c4b2112697d7a9b6f12b
Matt Enyeart <menyeart1@gmail.com>
2022-12-09 16:12:46 -0700

    Add a test for ship overlap

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -75,7 +75,20 @@ describe Board do
     expect(cell_3.ship).to be_an_instance_of(Ship)
     expect(cell_3.ship).to eq(cell_2.ship)
   end
-end
+
 
 # board.cells_hash["A1"]
 # expect(board.cells_hash["A1"]). to eq
+
+
+  it "will not allow ships to overlap" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2) 
+    expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+  end
+  
+end
+
+
