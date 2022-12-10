d6ce6227ecd938f9c5433fd14555c0f1352af7f9
Matt Enyeart <menyeart1@gmail.com>
2022-12-09 18:15:58 -0700

    Edit the board class for syntax

--- lib/board.rb
+++ lib/board.rb
@@ -69,7 +69,6 @@ class Board
       board_array << value.render(bool)
     end
 
-
     spaced_string = board_array.join('').gsub(/(.{1})/, '\1 ')  
 
     string1 = spaced_string.slice!(0, 8)
@@ -78,13 +77,8 @@ class Board
     string4 = spaced_string.slice!(0, 8)
 
     return "  1 2 3 4 \n" + "A #{string1}\n" + "B #{string2}\n" + "C #{string3}\n" + "D #{string4}\n"
-
-    
-    
+ 
   end  
-
-
-
 
 
 end  
